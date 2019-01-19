ea2a86fa90bdd704885e7a94f6f337aceaaa283e
이정윤 <leeyun1533@naver.com>
2019-01-19 19:59:04 +0900

    provider 및 router app.js에 제공

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles/reset.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import { stores } from "./stores"
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+
+ReactDOM.render(
+    <Provider {...stores}>
+     <BrowserRouter><App /></BrowserRouter>
+    </Provider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
